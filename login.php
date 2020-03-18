2817689a256b2029523bff37213909ce828cea83
Bryant052199 <32182989+Bryant052199@users.noreply.github.com>
2020-03-18 11:04:27 -0400

    Login page for Guest and Manager
    Contains a drop-down tab to chose either guest or manager.
    Redirects user to a Manger page and Guest page

--- login.php
+++ login.php
@@ -1,16 +1,58 @@
 <!DOCTYPE html>
+
 <html>
 <head>
 	<title>Registration system PHP and MySQL</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
+<?php
+
+
+$servername = "localhost";
+$username ="root";
+$password = "usbw";
+$dbname = "hotel";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+echo("connection");
+
+ if(isset($_POST['login_btn'])) //If user clicks login button
+
+{
+
+$user = $_POST['username'];
+$pass = $_POST['password'];
+$usertype = $_POST['usertype'];
+$query = "SELECT * FROM users WHERE username = $user and password = $pass
+and usertype = $usertype";
+$result = mysqli_query($conn,$query);
+
+
+	if($usertype == 'Manager'){
+		?>
+
+		<script type = "text/javascript">
+		<?php header('Location: http://' . $_SERVER['HTTP_HOST'] . '/manager.php');?>
+			<?php
+		}else{
+			?>
+			<?php echo 'orange' ?>
+			<script type = "text/javascript">
+			<?php header('Location: http://' . $_SERVER['HTTP_HOST'] . '/user.php');?>
+			<?php
+
+		}
+
+	}
+?>
+
+
 <body>
 	<div class="header">
 		<h2>Login</h2>
+
 	</div>
 	<form method="post" action="login.php">
 
-		<?php echo display_error(); ?>
 
 		<div class="input-group">
 			<label>Username</label>
@@ -20,11 +62,18 @@
 			<label>Password</label>
 			<input type="password" name="password">
 		</div>
+			<div class="input-group">
+		Select user type: <select name ="usertype">
+			<option value= "Manager">Manager</option>
+			<option value= "Guest">Guest</option>
+		</select>
+	</div>
+
 		<div class="input-group">
 			<button type="submit" class="btn" name="login_btn">Login</button>
 		</div>
 		<p>
-			Not a member? Register as guest <a href="action_page.php">Sign up</a>
+			Not a member? Register as guest <a href="login_action.php">Sign up</a>
 		</p>
 	</form>
 </body>
