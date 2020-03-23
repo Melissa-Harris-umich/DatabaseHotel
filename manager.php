7317e3d22a0314a955d958ea365f6ac6c7dea657
Bryant052199 <32182989+Bryant052199@users.noreply.github.com>
2020-03-23 15:17:29 -0400

    Update manager.php
    Hey you all this is the current status of the manager page. All it needs now are it's queries and some styling.

--- manager.php
+++ manager.php
@@ -1,31 +1,127 @@
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta value="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manager Site</title>
     <link rel="stylesheet" type="text/css" href="/github/databasehotel/style.css">
- 
+
 </head>
 <body>
-<?php
-   $con = mysql_connect('localhost', 'root','usbw'); /*Function used
-   to connect to database*/
-   $db = mysql_select_db('421 Database');
-   if($con) //Verify  connection to database
-   {
-    //echo "Successfully connected to the database";
-  }
-  else {
-    die("Error");
-  }
 
-
-?>
 
 
 
 <h1>Manager Site</h1>
-    
+<form action = '/DatabaseHotel/manager.php' method = "post"
+<div>
+<p>Please select at least one option<p>
+<p><input type="checkbox"name="language[]" value="Update Hotel" />Update Hotel</p>
+<p><input type="checkbox"name="language[]" value="Delete Room"/>Delete a Room</p>
+<p><input type="checkbox"name="$language" value="Add Hotel"/>Add a Hotel</p>
+<p><input type="checkbox"name="language[]" value="Delete Hotel"/>Delete a Hotel</p>
+<p><input type="checkbox"name="language[]" value="Add Room Type"/>Add a Room Type</p>
+<p><input type="checkbox"name="language[]" value="Add Occupants"/>Add more Occupants</p>
+<p><input type="checkbox"name="language" value="Revenue"/>Generate Revenue</p>
+<p><input type="submit" name="submit" value="Submit" /></p>
+</form>
+</body>
+</html>
+
+
+<?php
+if(isset($_POST["submit"]))  //if user clicks the submit button
+{
+     $language = $_POST['language'];
+
+  if(!empty($_POST["language"]))  //if user clicks on a check box
+  {
+    echo '<h3>The following actions have been selected<h3>';
+    foreach($_POST["language"] as $key=> $values)  /* $key => Values allows for
+    more than one value that has been checked to be stored.*/
+    $key => Values allows for mor
+    {
+          echo $values.", ";
+    }
+
+    if($values == "Update Hotel")
+    {
+
+      /*Redirect to new page to update values within
+      Hotel Relation
+      */
+    }
+    if($values == "Delete Room")
+    {
+      /* Querey database
+
+      */
+
+    }
+    if($values == "Add Hotel")
+    {
+      /* Querey database
+
+      */
+
+    }
+    if($values == "Delete Hotel")
+    {
+      /* Querey database
+
+      */
+
+    }
+    if($values == "Add Room Type")
+    {
+      /* Querey database
+
+      */
+
+    }
+    if($values == "Delete Room")
+    {
+      /* Querey database
+
+      */
+
+    }
+    if($values == "Add Occupants")
+    {
+      /* Querey database
+
+      */
+    }
+    if($values == "Revenue")
+    {
+      /* Querey database
+
+      */
+
+    }
+
+
+  }else {
+    echo 'Please check at least one option';
+  }
+}
+
+
+
+ ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
