0540a39a2fc9c81cd05730c1a71beebd94f4a168
Melissa Harris <53122980+Melissa-Harris-umich@users.noreply.github.com>
2020-03-30 21:42:36 -0400

    guest page

--- guest.php
+++ guest.php
@@ -8,7 +8,8 @@
     <!-- Bootstrap CSS -->
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
    <link rel="stylesheet" type="text/css" href="/github/databasehotel/style.css">
-    <title>Hello, world!</title>
+  
+    <title>Guest Page</title>
   </head>
 
 
@@ -43,14 +44,40 @@
 
 <style>
 
+
+/*For Guest Background*/
 #header {
-    background: url(assets/img/hero/h1_hero.jpg);
-    height: 100vh;
+     background: url(assets/img/hero/guestbg.jpg)no-repeat; 
+     background-attachment: fixed;
+     height: 100vh;
+     background-size: 100%;
+
+
 }
+label{
+  color:rgb(231, 202, 173);
+  font-weight: bold;
+}
+#myDIV {
+  width: 100%;
+  padding: 5px 0;
+  text-align: center;
+  margin-top: 5px;
+  display: none;
+}
+
+#Del {
+  width: 100%;
+  padding: 5px 0;
+  text-align: center;
+  margin-top: 5px;
+  display: none;
+}
+
 
 </style>
 <header>
-
+<!--Top Navigation bar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
           <a class="navbar-brand" href="#">HOTEL</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -72,68 +99,126 @@
             </ul>
           </div>
         </nav>
-
-
+<!-- Top Navigation Cod Ends Here -->
+</header>
 
             <!-- id in section given header for meaning, to make more meaningful -->
-    <section id="header" class="jumbotron text-center">
+    <section id="header" class="jumbotron text-center mb-0">
       <div class="center">
 
 
+    <h1>Choose an Option</h1>
 
-     <form method="post">
-                    <!-- select in date -->
-                   
-                        <!-- select out date -->
-                       <div>
-                            <span> Check In Date:</span>
-                       
-                     
-                            <input id="datepicker1" name="check_in" type='date' placeholder="10/12/2020"  />
-                        
-                   </div>
-                   </br>
-                    <!-- Single Select Box -->
-                    <div>
-                            <span>Check OutDate:</span>
-                        
-                        
-                            <input id="datepicker2" name="check_out"  type='date' placeholder="12/12/2020" />
-                        </div>
-                        </br>
-                   <div>
-                    <!-- Single Select Box -->
-                  
-                       
-                            <span>Number of Occupants:</span>
-                       
-                      
-                            <form action="#">
-                                
-                                    <select name="select" id="select1" name="numOccupants">
-                                        <option value="">1</option>
-                                        <option value="">2</option>
-                                        <option value="">3</option>
-                                        <option value="">4</option>
-                                        <option value="">5</option>
-                                        <option value="">6</option>
-                                    </select>
-                                     </div>
-                                     </br>
-                                
-                            </form>
-                        </div>
-                   </div>
 
-                    <div class="single-select-box pt-45 mb-30">
-</br>
-                     <input type='submit' href="#" class="btn select-btn" name="bookBtn">
-                   </div>
-                   </form> 
-      </div>
+<form method="post">
+ <!-- Button Options -->
+ <button type="button" id= "Book"  class="btn btn-dark" onclick="myFunction(this.id)" >Book a room</button>
+ <button type="button" id="Cancel" class="btn btn-dark" onclick="myFunction(this.id)">Cancel your reservation</button>
+ <button type="button" id="Delete" class="btn btn-danger" onclick="myFunction(this.id)">Delete your account</button>
 
-    </section>
 
-</header>
-</nav>
+ <!---------------------------------Book a room menu functions  -->
+ <div id="myDIV" display="none">
+ <label for="num_guests">Number of guests: </label>
+<select id="num_guests" name="num_guests">
+  <option value="1">1</option>
+  <option value="2">2</option>
+  <option value="3">3</option>
+  <option value="4">4</option>
+</select>
+<label for="checkin">Check in date:</label>
+<input type ="date" name="checkin">
+<label for="checkout">Check out date:</label>
+<input type ="date" name="checkout">
+</div>
+<!-------------------------- Book a room menu options end here -->
+
+
+<!------------------------- Cancel Option delete from table here -->
+<div id="Del" display="none">
+  <label for="Del">Please choose reservation to cancel:</label>
+
+  <?php
+//Get table data
+$query = "SELECT * FROM guest ";//Display info within Database
+$result =mysql_query($query);
+?>
+
+  <table class="table table-sm table-dark" id="Guest_Table">
+
+    <tr>	
+    <thead><h3>Your Reservations</h3></th>
+    </tr>
+    <th scope="col"> ID</th>
+    <th scope="col">First Name</th>	
+    <th scope="col">Last Name</th>	
+    <th scope="col">Address</th>
+    <th scope="col">City</th>
+    <th scope="col">Check to Cancel</th>
+    </tr>	
+</thead>
+<tbody>
+    <tr>	
+    <?php	
+   
+     //Table data taken from assigned variables, displays guest data in Front-end table  
+     while($row = mysql_fetch_assoc($result)) 
+     {
+       ?>
+  
+     <td><?php echo  $row['GuestID']; ?></td>
+     <td><?php echo  $row['GFname']; ?></td>
+     <td><?php echo  $row['GLname']; ?></td>
+     <td><?php echo  $row['GAddress']; ?></td>
+     <td><?php echo  $row['GCity']; ?></td>
+     <td><input type="checkbox"></td>
+     </tr>
+         
+         <?php
+        }
+            ?>
+         </tbody>
+     </table>
+ </div>
+<!-------------------------- Cancel a room menu options end here -->
+
+
+
+
+
+<!-- Javascript -->
+<script>
+// Function for toggling menu after button click here
+function myFunction(id) {
+  
+  if(id =="Book") {
+    var x = document.getElementById("myDIV");
+    var del = document.getElementById("Del");
+    del.style.display = "none";
+
+  }
+  
+  if(id =="Cancel") {
+  var x = document.getElementById("Del");
+  var b = document.getElementById("myDIV");
+  b.style.display = "none";
+  }
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+
+  
+}
+
+// Toggle function end
+
+
+</script>
+<!-- End of Javascript code -->
+
+
+</body>
 </html>
