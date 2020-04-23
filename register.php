c206ca941c2546123b67aece6b2aede8b8ec2a08
Melissa Harris <53122980+Melissa-Harris-umich@users.noreply.github.com>
2020-04-22 22:58:36 -0400

    Added style to register

--- register.php
+++ register.php
@@ -1,11 +1,41 @@
-<!Doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>Guest Table</title>
-    <link rel="stylesheet" type="text/css" href="/github/databasehotel/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Register</title>
+
+<style>
+html,body{
+background-image: url(img/guestbg.jpg);
+background-size: cover;
+background-repeat: no-repeat;
+height: 100%;
+font-family: 'Numans', sans-serif;
+}
+.centered-form{
+	margin-top: 60px;
+}
+
+.centered-form .panel{
+	background: rgba(255, 255, 255, 0.8);
+  box-shadow: rgba(0, 0, 0, 0.3) 20px 20px 20px;
+  
+
+}
+.btn-c {
+ background-color:#dab31e;
+
+}
+</style>
 </head>
 <body>
- <?php
+
+
+
+<!------PHP----------->
+
+<?php
    $con = mysql_connect('localhost', 'root','usbw'); /*Function used
    to connect to database*/
    $db = mysql_select_db('hotel_db');
@@ -26,49 +56,115 @@ $sql = "INSERT INTO Registration (username, password, User_type, GFname, GLname,
 
 
 if(mysql_query($sql,$con )){
-  echo "Records added successfully.";
+
+  echo 
+  "<script type=\"text/javascript\">".
+  "window.alert('Records added successfully.');".
+  "top.location = 'index.php';".
+  "</script>"; 
+
+   exit; 
+
+
+  header('Location: index.php');
 } else{
+  "<script type=\"text/javascript\">".
+  "window.alert('Unsuccessful Try again.');".
+  "top.location = 'test.php';".
+  "</script>"; 
   echo "ERROR: Could not able to execute $sql. " . mysql_error($con);
 }
 }
 
 ?>
-
-
-
-<br />
-
-<h1>Hotel Database</h1>
-
-
-<!---Input form here HTML ---->
-<form action="register.php" method="post" name="action" value="updateG">
-  <!---Name of table column goes in the name= field-->
-
-  <div>
-  <label>Username:</label>
-  <input type="text" id="username" name="username"><br>
-  <label>Password:</label>
-  <input type="password" id="password" name="password"><br>
-  <label>Usertype:</label>
-  <input type="text" id="usertype" name="usertype" placeholder = "Guest/Manager"><br>
-  <label>First name:</label>
-  <input type="text" id="Fname" name="GFname"><br>
-  <label>Last name:</label>
-  <input type="text" id="Lname" name="GLname"><br>
-  <label>Address:</label>
-  <input type="text" id="address" name="GAddress"><br>
-  <label>City:</label>
-  <input type="text" id="city" name="GCity"><br>
-  <input type="submit" value="Submit" name='Reg_Insert'>
-</div>
-</form>
+<!--PHP-->
 
 
 
 
+  
+
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="container">
+        <div class="row centered-form">
+        <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+        	<div class="panel panel-default">
+        		<div class="panel-heading">
+			    		<h3 class="panel-title">Registration</h3>
+			 			</div>
+			 			<div class="panel-body">
+             <form action="test.php" method="post" name="action" value="updateG">
+			    			<div class="row">
+
+
+			    		
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="username" name="username" class="form-control input-sm" placeholder="Username">
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="password" name="password" name="password"  class="form-control input-sm" placeholder="Password">
+			    					</div>
+			    				</div>
+			    			</div>
+			    			
+
+                <div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+                    <div class="form-group">
 
 
 
-</body>
+                   
+                    
+			                <input type="text" name="GFname" class="form-control input-sm" placeholder="First Name">
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="text" name="GLname" class="form-control input-sm" placeholder="Last Name">
+			    					</div>
+			    				</div>
+			    			</div>
+
+			    			<div class="form-group">
+			    				<select type="dropdown" name="usertype"  class="form-control input-sm" placeholder="Guest/Manager">
+
+                 
+  <option value="Manager">Manager</option>
+  <option value="Guest">Guest</option>
+
+</select>
+			    			</div>
+
+                
+			    			<div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="text" name="GAddress" class="form-control input-sm" placeholder="Address">
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="text" name="GCity" class="form-control input-sm" placeholder="City">
+			    					</div>
+			    				</div>
+			    			</div>
+			    			<input type="submit" value="Register" name='submit' class="btn btn-c btn-block">
+			    		
+			    		</form>
+			    	</div>
+	    		</div>
+    		</div>
+    	</div>
+    </div>
+
+
+    </body>
 </html>
